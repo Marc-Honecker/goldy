d5ec07c4b7ebe80b1b7ba84fb418b668589c190a
Marc Honecker <marc@honecker-mail.de>
2024-04-14 11:30:25 +0200

    added test

--- crates/goldy_thermo/src/langevin.rs
+++ crates/goldy_thermo/src/langevin.rs
@@ -57,3 +57,57 @@ where
         Self::new()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use goldy_storage::{
+        atom_type::AtomTypeBuilder,
+        atom_type_store::AtomTypeStoreBuilder,
+        vector::{Forces, Velocities},
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_langevin() {
+        // some parameters
+        let num_atoms = 1_000;
+        let temp = 1.0;
+        let dt = 0.001;
+        let mass = 39.95;
+        let gamma = 0.01;
+
+        // the test-object
+        let mut langevin = Langevin::new();
+
+        // the atoms
+        let v = Velocities::<f32, 3>::zeros(num_atoms);
+        let mut f = Forces::zeros(num_atoms);
+        let types = AtomTypeStoreBuilder::default()
+            .add_many(
+                AtomTypeBuilder::default()
+                    .mass(mass)
+                    .gamma(gamma)
+                    .build()
+                    .unwrap(),
+                num_atoms,
+            )
+            .build();
+
+        // performing one step in the thermostat
+        langevin.thermo(&mut f, &v, &types, temp, dt);
+
+        // all forces must be in [-sqrt(6*m*gamma*T), sqrt(6*m*gamma*T)]
+        let bound = (6.0 * mass * gamma / dt * temp).sqrt();
+        assert!(f
+            .iter()
+            .all(|f| f.iter().all(|x| (-bound..bound).contains(x))));
+
+        // Some directions need to be negative ..
+        assert!(f.iter().any(|f| f.iter().any(|&x| x < 0.0)));
+        // ... and some positive.
+        // In fact, this test ensures, that the thermostat really
+        // did something.
+        assert!(f.iter().any(|f| f.iter().any(|&x| x > 0.0)));
+    }
+}
