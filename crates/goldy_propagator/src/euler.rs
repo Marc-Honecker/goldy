8d5c7dc569ca2bf8a6da7f82c374600821b864a1
Marc Honecker <marc@honecker-mail.de>
2024-04-22 15:15:49 +0200

    added first test

--- crates/goldy_propagator/src/euler.rs
+++ crates/goldy_propagator/src/euler.rs
@@ -65,3 +65,77 @@ impl Propagator for Euler {
         potential_energy
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use assert_approx_eq::assert_approx_eq;
+    use goldy_box::SimulationBoxBuilder;
+    use goldy_potential::harmonic_oscillator::HarmonicOscillator;
+    use goldy_storage::{
+        atom_store::{AtomStore, AtomStoreBuilder},
+        atom_type::AtomTypeBuilder,
+        atom_type_store::AtomTypeStoreBuilder,
+        vector::{Forces, Positions, Velocities},
+    };
+    use goldy_thermo::langevin::Langevin;
+    use nalgebra::Matrix3;
+
+    use super::*;
+
+    #[test]
+    fn test_euler_without_everything() {
+        // defining our big AtomStore.
+        let num_atoms = 1;
+        let mut atom_store: AtomStore<f32, 3> = AtomStoreBuilder::default()
+            .positions(Positions::zeros(num_atoms))
+            .velocities(Velocities::zeros(num_atoms))
+            .forces(Forces::zeros(num_atoms))
+            .atom_types(
+                AtomTypeStoreBuilder::default()
+                    .add(
+                        AtomTypeBuilder::default()
+                            .mass(39.95)
+                            .damping(0.01)
+                            .build()
+                            .unwrap(),
+                    )
+                    .build(),
+            )
+            .build()
+            .unwrap();
+
+        // doesn't do anything, but we need it
+        let sim_box = SimulationBoxBuilder::default()
+            .hmatrix(Matrix3::from_diagonal_element(10.0))
+            .boundary_type(goldy_box::BoundaryTypes::Open)
+            .build()
+            .unwrap();
+
+        // propating one step
+        let potential_energy = Euler::integrate::<f32, 3, HarmonicOscillator<f32>, Langevin<f32>>(
+            &mut atom_store,
+            &sim_box,
+            None,
+            None,
+            0.01,
+            1.0,
+        );
+
+        // potential_energy must be None, because we didn't provide any Potential nor Thermostat.
+        assert_eq!(potential_energy, None);
+
+        // testing, if everything stayed zero
+        atom_store
+            .x
+            .iter()
+            .for_each(|x| x.iter().for_each(|x| assert_approx_eq!(x, 0.0)));
+        atom_store
+            .v
+            .iter()
+            .for_each(|v| v.iter().for_each(|x| assert_approx_eq!(x, 0.0)));
+        atom_store
+            .f
+            .iter()
+            .for_each(|f| f.iter().for_each(|x| assert_approx_eq!(x, 0.0)));
+    }
+}
