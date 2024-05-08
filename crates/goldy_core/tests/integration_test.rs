506d4dd6cf6c9966a4b0a923e7e7edc7e1bd2791
Marc Honecker <marc@honecker-mail.de>
2024-05-08 15:23:34 +0200

    updated test

--- crates/goldy_core/tests/integration_test.rs
+++ crates/goldy_core/tests/integration_test.rs
@@ -1,74 +1,65 @@
 #[test]
 fn ideal_gas() {
     use assert_approx_eq::assert_approx_eq;
+    use nalgebra::Vector3;
 
     use goldy_core::{
-        storage::{
-            atom_type::AtomTypeBuilder,
-            atom_type_store::AtomTypeStoreBuilder,
-            vector::{Forces, Positions, Velocities},
-        },
-        thermo::{langevin::Langevin, ForceDrivenThermostat},
+        force_update::ForceUpdateBuilder,
+        propagator::{velocity_verlet::VelocityVerlet, Propagator},
+        simulation_box::BoundaryTypes,
+        storage::atom_type::AtomTypeBuilder,
+        system::System,
+        thermo::langevin::Langevin,
     };
 
     // simulation parameters
     let dt = 0.01;
-    let temp = 70.0;
-    let runs = 200_000;
+    let temp = 1.0;
+    let runs = 500_000;
 
     // Argon
     let at = AtomTypeBuilder::default()
-        .damping(0.005)
+        .damping(0.01)
         .mass(39.95)
         .build()
         .unwrap();
 
     // the atoms
-    let num_atoms = 1_000;
-    let mut x = Positions::<f32, 3>::new_gaussian(num_atoms, 0.0, 1.0);
-    let mut v = Velocities::zeros(num_atoms);
-    let mut f = Forces::zeros(num_atoms);
-    let types = AtomTypeStoreBuilder::default()
-        .add_many(at, num_atoms)
-        .build();
+    let mut system = System::new_cubic(Vector3::new(10, 10, 10), 2.0, BoundaryTypes::Open, at);
 
     // thermostat
-    let mut langevin = Langevin::new();
+    let langevin = Langevin::new();
+
+    // creating the updater
+    let mut updater = ForceUpdateBuilder::default()
+        .thermostat(Box::new(langevin))
+        .build();
 
     // kinetic energy moments
     let mut tkin_1 = 0.0;
 
     // the main MD-loop
     for _ in 0..runs {
-        // computing the forces (for ideal gas)
-        langevin.thermo(&mut f, &v, &types, temp, dt);
-
-        // propagating in time
-        //
-        // converting the forces to accelerations
-        f.iter_mut().zip(&types).for_each(|(f, t)| *f /= t.mass());
-        // updating the velocities
-        v.iter_mut().zip(&f).for_each(|(v, f)| *v += *f * dt);
-        // updating the positions
-        x.iter_mut().zip(&v).for_each(|(x, v)| *x += *v * dt);
+        // propagating the system in time
+        VelocityVerlet::integrate(&mut system.atoms, &system.sim_box, &mut updater, dt, temp);
 
         // measuring the kinetic energy
         let tkin_mean = 0.5
-            * v.iter()
-                .zip(&types)
+            * system
+                .atoms
+                .v
+                .iter()
+                .zip(&system.atoms.atom_types)
                 .map(|(&v, &t)| t.mass() * v.dot(&v))
                 .sum::<f32>();
-
-        // initliazing the forces.
-        f.set_to_zero();
 
         // updating the moments
         tkin_1 += tkin_mean;
     }
 
-    tkin_1 /= (runs * num_atoms) as f32;
+    tkin_1 /= (runs * system.number_of_atoms()) as f32;
 
     // this should hold
     let analytical_solution = 1.5 * temp;
-    assert_approx_eq!(analytical_solution, tkin_1, 5e-3 * analytical_solution);
+    assert_approx_eq!(analytical_solution, tkin_1, 1e-3 * analytical_solution);
 }
