abdf10aeaa086315fc3fe819ceee615002bd9bc2
Marc Honecker <marc@honecker-mail.de>
2024-08-04 09:44:36 +0200

    Added support for random Positions

--- crates/goldy_core/src/storage/positions.rs
+++ crates/goldy_core/src/storage/positions.rs
@@ -1,4 +1,5 @@
 use nalgebra::SVector;
+use rand::distributions::Uniform;
 use rand::SeedableRng;
 use rand_chacha::ChaChaRng;
 use rand_distr::{Distribution, Normal};
@@ -8,7 +9,7 @@ use crate::{Real, storage::vector::Positions};
 impl<T, const D: usize> Positions<T, D>
 where
     T: Real,
-    rand_distr::StandardNormal: rand_distr::Distribution<T>,
+    rand_distr::StandardNormal: Distribution<T>,
 {
     pub fn new_gaussian(n: usize, mean: T, std_dev: T) -> Self {
         let mut rng = ChaChaRng::from_entropy();
@@ -19,5 +20,38 @@ where
             .collect();
 
         Self { data: x }
+    }
+}
+
+impl<T, const D: usize> Positions<T, D>
+where
+    T: Real + rand_distr::uniform::SampleUniform,
+{
+    pub fn new_uniform(lengths: &SVector<T, D>, n: usize) -> Self {
+        // initializing the RNG
+        let mut rng = ChaChaRng::from_entropy();
+        // initializing the uniform samplers
+        let samplers: Vec<_> = lengths
+            .iter()
+            .map(|&x| {
+                Uniform::new(
+                    num_traits::Float::min(T::zero(), x),
+                    num_traits::Float::max(T::zero(), x),
+                )
+            })
+            .collect();
+
+        // creating the positions
+        let mut x = Self::zeros(n);
+
+        x.iter_mut().for_each(|x| {
+            x.iter_mut()
+                .zip(&samplers)
+                .for_each(|(direction, sampler)| {
+                    *direction = sampler.sample(&mut rng);
+                })
+        });
+
+        x
     }
 }
