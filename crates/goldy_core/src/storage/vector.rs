cb7a0aad8adf254a8d3aab6d0ce8528fc6aa00fc
Marc Honecker <marc@honecker-mail.de>
2024-09-09 14:16:28 +0200

    Minor test fix

--- crates/goldy_core/src/storage/vector.rs
+++ crates/goldy_core/src/storage/vector.rs
@@ -99,7 +99,7 @@ macro_rules! generate_structs {
                         contents.push_str(format!("{: >10}{: >10}", id + 1, at.id()).as_str());
 
                         x.iter()
-                            .for_each(|x| contents.push_str(format!("{x: >50.6}").as_str()));
+                            .for_each(|x| contents.push_str(format!("{x: >20.6}").as_str()));
                         contents.push('\n');
                     });
 
